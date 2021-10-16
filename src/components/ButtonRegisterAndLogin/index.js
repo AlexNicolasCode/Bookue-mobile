23220c37f6e1dfd3a8bc3216da80fd75fbd7f07b
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-16 16:02:27 -0300

    style: user can see button in screens

--- src/components/ButtonRegisterAndLogin/index.js
+++ src/components/ButtonRegisterAndLogin/index.js
@@ -9,3 +9,20 @@ export default function ButtonRegisterAndLogin({ isLogin }) {
         </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: "2.5px",
+        justifyContent: 'center',
+        width: 125,
+        height: 40,
+        marginTop: 8,
+        backgroundColor: globalColors.primary
+    },
+    button__text: {
+        textAlign: 'center',
+        fontWeight: '500',
+        fontSize: 20,
+        color: globalColors.white
+    }
+})
